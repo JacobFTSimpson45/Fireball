b2315ddb35c5b009703233c28beaab942b5e8363
JacobFTSimpson45 <jacob.f.t.simpson.45@gmail.com>
2024-08-28 13:04:45 +1200

    Fixed score counter

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    public int objectsHit;
+    public int objectsDestroyed;
     public int currentHealth;
-    public float currentTime = 60;
+    public int radius = 50;
+    public float currentTime = 0;
     public UiManager uiManager;
 
     public void Update()
     {
-        currentTime -= Time.deltaTime;
+        currentTime += Time.deltaTime;
         uiManager.UpdateTimer(currentTime);
 
-
-        uiManager.UpdateScore(objectsHit);
-
-        Health hp = gameObject.GetComponent<Health>();
-        currentHealth = hp.health;
+        Health PlayerHP = gameObject.GetComponent<Health>();
+        currentHealth = PlayerHP.health;
         uiManager.UpdateHealth(currentHealth);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider col in colliders)
+        {
+            if (col.tag == "Enemy")
+            {
+                Health enemyHealth = col.gameObject.GetComponent<Health>();
+                if (enemyHealth.health <= 0)
+                {
+                    objectsDestroyed++;
+                }
+            }
+        }
+
+        uiManager.UpdateScore(objectsDestroyed);
     }
 }
